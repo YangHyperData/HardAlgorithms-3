e7e9bce603eab59224510224456854574228af69
YangStone <131573711+YangHyperData@users.noreply.github.com>
2025-04-30 15:27:03 +0700

    2028A - Alice's Adventures in "Chess"

---
+++ 2028A.cpp
@@ -0,0 +1,216 @@
+/* YangStone Come To Play */
+
+#include <vector>
+#include <bits/stdc++.h>
+#include <conio.h>
+#include <algorithm>
+using namespace std;
+
+#define RFOR(i, a, b) for (int(i) = (a) - 1; (i) >= (b); --(i))
+#define nl << '\n'
+#define vti vector<int>
+#define vtll vector<long long>
+#define yep cout << "YES" nl
+#define nope cout << "NO" nl
+#define ll long long
+#define lg long
+#define db double
+#define scanv(x)      \
+	for (auto &i : x) \
+	cin >> i
+// #define MOD 998244353
+#define dbg(x)         \
+	cerr << #x << " "; \
+	_print(x);         \
+	cerr << endl;
+#define pb push_back
+#define bg begin()
+#define ed end()
+#define rbg rbegin()
+#define red rend()
+#define dbg(x...)        \
+	cerr << #x << " : "; \
+	_print(x)
+#define uceil(a, b) ((a + b - 1) / (b))
+#define dbg(...)
+#define RFOR(i, a, b) for (int(i) = (a) - 1; (i) >= (b); --(i))
+#define sp << " "
+#define fr(i, l, r) for (int i = l; i <= r; i++)
+#define FOR(i, a, b) for (int(i) = (a); (i) < (b); ++(i))
+#define fl(i, r, l) for (int i = r; i >= l; i--)
+#define wh while
+#define fat(x, a) for (auto &x : a)
+#define srt(x) sort(x.bg, x.ed)
+#define printall(a)       \
+	for (auto &(i) : (a)) \
+	cout << i << ' '
+#define FOR(i, a, b) for (int(i) = (a); (i) < (b); ++(i))
+#define nlp(i, n) for (int i = 0; i < (int)(n); ++i)
+#define rsrt(x) sort(x.rbg, x.red)
+#define all(r) r.begin(), r.end()
+#define vi vector<int>
+// const int MAXN = 4e5 + 5;
+#define forn(i, n) for (int i = 0; i < int(n); ++i)
+#define rall(r) r.rbegin(), r.rend()
+#define fast_out()                    \
+	ios_base::sync_with_stdio(false); \
+	cin.tie(NULL)
+// const int mod = 1e9 + 7;
+// const int N = 1e5 + 10;
+const ll INF = 1e9 + 7;
+#define FORALL(i, a) for (auto &(i) : (a))
+#define yn(f) f ? cout << "YES\n" : cout << "NO\n"
+// void file() {freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);}
+int bin_pow(int a, int b, int mod) // luy thua nhi phan
+{
+	if (b == 0)
+		return 1;
+	int tmp = bin_pow(a, b / 2, mod);
+	if ((b & 1) == false)
+	{
+		return (tmp * tmp) % mod;
+	}
+	return (a * (tmp * tmp) % mod) % mod;
+}
+
+vector<int> factorize(int n)
+{
+	vector<int> res;
+	for (int i = 2; i * i <= n; i++)
+	{
+		while (n % i == 0)
+		{
+			res.push_back(i);
+			n /= i;
+		}
+	}
+	if (n != 1)
+		res.push_back(n);
+	return res;
+}
+//* Delete
+/*var a = document.querySelectorAll(".MJX_Assistive_MathML")
+a.forEach(s=> s.remove())
+*/
+bool compare(string &a, string &b)
+{
+	return a + b > b + a;
+}
+bool isPerfectSquare(ll n)
+{
+	ll temp = sqrt(n);
+
+	if (temp * temp == n)
+		return true;
+	return false;
+}
+bool check(char r)
+{
+	return (r == 'a' || r == 'e');
+}
+bool isPerfectSquare(int n)
+{
+	int rt = sqrt(n);
+	return (rt * rt == n);
+}
+bool isPrime(int n)
+{
+	for (int i = 2; i * i <= n; ++i)
+	{
+		if (n % i == 0)
+			return false;
+	}
+
+	return true;
+}
+bool check(long x, long y, long a, long b, long xt, long yt)
+{
+	if (xt == 0 && yt == 0)
+	{
+		return x == a && y == b;
+	}
+	else if (xt == 0)
+	{
+		return (x == a && (b - y) % yt == 0 && (b - y) / yt > 0);
+	}
+	else if (yt == 0)
+	{
+		return (y == b && (a - x) % xt == 0 && (a - x) / xt > 0);
+	}
+	else
+	{
+		return (a - x) % xt == 0 && (b - y) % yt == 0 && (a - x) / xt >= 0 && (a - x) / xt == (b - y) / yt;
+	}
+}
+void solve()
+{
+	long n, a, b;
+	cin >> n >> a >> b;
+	string s;
+	cin >> s;
+	long xt(0), yt(0);
+	for (long p = 0; p < s.size(); p++)
+	{
+		if (s[p] == 'N')
+		{
+			++yt;
+		}
+		else if (s[p] == 'S')
+		{
+			--yt;
+		}
+		else if (s[p] == 'E')
+		{
+			++xt;
+		}
+		else if (s[p] == 'W')
+		{
+			--xt;
+		}
+	}
+
+	long x(0), y(0);
+	bool ans(check(0, 0, a, b, xt, yt));
+	for (long p = 0; p < s.size() && !ans; p++)
+	{
+		if (s[p] == 'N')
+		{
+			++y;
+		}
+		else if (s[p] == 'S')
+		{
+			--y;
+		}
+		else if (s[p] == 'E')
+		{
+			++x;
+		}
+		else if (s[p] == 'W')
+		{
+			--x;
+		}
+		if (check(x, y, a, b, xt, yt))
+		{
+			ans = true;
+		}
+	}
+
+	cout << (ans ? "YES" : "NO") << endl;
+}
+
+int main()
+{
+	fast_out();
+
+	int t = 1;
+	cin >> t;
+
+	wh(t--)
+	{
+
+		solve();
+		// cout << endl;
+	}
+
+	return 0;
+}
